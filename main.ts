bebfc86d35066e4b60bd06c2fb67e1d40e7a17bb
tmtsr <117850984+tmtsr@users.noreply.github.com>
2023-02-23 16:04:01 +0100

    Initiële bestanden voor MakeCode-project

---
+++ main.ts
@@ -0,0 +1,14 @@
+basic.clearScreen()
+basic.showLeds(`
+    . . . . .
+    . . . . .
+    # # # . .
+    . . . . .
+    # . # # #
+    `)
+let x = 0
+let y = 0
+led.plot(x, y)
+basic.forever(function () {
+	
+})
