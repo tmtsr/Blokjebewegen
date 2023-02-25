490096fb21270e0f028383f993df7ece4403c41b
tmtsr <117850984+tmtsr@users.noreply.github.com>
2023-02-25 11:14:14 +0100

    Spel actief of winscherm

--- main.ts
+++ main.ts
@@ -1,48 +1,69 @@
-input.onButtonPressed(Button.A, function () {
-    led.unplot(x, y)
-    x += -1
+function start_spel () {
+    basic.clearScreen()
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        # # # . .
+        # . . . .
+        # . # # #
+        `)
+    x = 0
+    y = 0
     led.plot(x, y)
+    spel_actief = 1
+}
+input.onButtonPressed(Button.A, function () {
+    if (spel_actief == 1) {
+        led.unplot(x, y)
+        x += -1
+        led.plot(x, y)
+    } else {
+        start_spel()
+    }
 })
 input.onButtonPressed(Button.AB, function () {
-    led.unplot(x, y)
-    y += 1
-    led.plot(x, y)
+    if (spel_actief == 1) {
+        led.unplot(x, y)
+        y += 1
+        led.plot(x, y)
+    } else {
+        start_spel()
+    }
 })
 input.onButtonPressed(Button.B, function () {
-    led.unplot(x, y)
-    x += 1
-    led.plot(x, y)
+    if (spel_actief == 1) {
+        led.unplot(x, y)
+        x += 1
+        led.plot(x, y)
+    } else {
+        start_spel()
+    }
 })
+let spel_actief = 0
 let y = 0
 let x = 0
-basic.clearScreen()
-basic.showLeds(`
-    . . . . .
-    . . . . .
-    # # # . .
-    # . . . .
-    # . # # #
-    `)
-x = 0
-y = 0
-led.plot(x, y)
-let spel_actief = 1
+start_spel()
 basic.forever(function () {
-    basic.pause(1000)
-    led.unplot(x, y)
-    y += 1
-    led.plot(x, y)
-    if (y == 2 && x < 3) {
-        basic.showIcon(IconNames.Sad)
-    } else {
-        if (y == 4) {
-            if (x == 1) {
-                basic.pause(1000)
-                basic.showIcon(IconNames.Happy)
-            } else {
-                basic.pause(1000)
-                basic.showIcon(IconNames.Sad)
+    if (spel_actief == 1) {
+        basic.pause(1000)
+        led.unplot(x, y)
+        y += 1
+        led.plot(x, y)
+        if (y == 2 && x < 3) {
+            basic.showIcon(IconNames.Sad)
+            spel_actief = 0
+        } else {
+            if (y == 4) {
+                if (x == 1) {
+                    basic.showIcon(IconNames.Happy)
+                    spel_actief = 0
+                } else {
+                    basic.showIcon(IconNames.Sad)
+                    spel_actief = 0
+                }
             }
         }
+    } else {
+    	
     }
 })
